e3c8210ac07a279c18a9da0c62b849955184ef45
richard-mante <richardmanteamo@gmail.com>
2024-05-06 00:53:34 +0000

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Location = New Point(12, 120)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(776, 120)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Bio Data"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(76, 291)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(122, 38)
+        Button1.TabIndex = 1
+        Button1.Text = "Kick Ass"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
+
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Hello, you successfully killed that bitch")
+        MsgBox("You have been upgraded to the next level")
+        MsgBox("Kill the mafia king to be the next Yakuza boss")
+    End Sub
+
+
+End Class
